126c914d79b5d91dfa0713f30a6c434c2cb93411
Justfindingsomethinginlife <golfgamertv192@gmail.com>
2024-02-21 19:44:33 +0700

    Read from hex file

--- BinRunner/BINRUN.c
+++ BinRunner/BINRUN.c
@@ -7,10 +7,27 @@ int main()
     FILE *fp;
     fp = fopen("test.bin","rb");
     fseek(fp,0,SEEK_SET);
-    for(int i=0;i<10;i++)
+    for(int i=0;i<20;i++)
     {
 	fread(b,1,1,fp);
 	printf("%x ",*b);
+	switch(*b)
+		{	
+			case 0x01:
+				printf("SET ");
+				fread(b,1,1,fp);
+				printf("%x ",*b);
+				fread(b,1,1,fp);
+				printf("%x ",*b);
+				printf("\n");
+				break;
+			case 0x02:
+				printf("GET ");
+				fread(b,1,1,fp);
+				printf("%x ",*b);
+				printf("\n");
+				break;
+		}	
     }
     
     fclose(fp);
