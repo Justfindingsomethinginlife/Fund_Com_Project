e5dc8bda5b3d73e5fe87cbac9a7610679e7add96
Justfindingsomethinginlife <golfgamertv192@gmail.com>
2024-02-21 11:08:50 +0700

    read on by one now

--- BinRunner/BINRUN.c
+++ BinRunner/BINRUN.c
@@ -3,16 +3,15 @@
 
 int main()
 {	
-    int b[20];
+    int b[3];
     FILE *fp;
     fp = fopen("test.bin","rb");
     fseek(fp,0,SEEK_SET);
-    //for(int i=0;i<2;i++)
-    	//printf("%d",fread(b,1,1,fp));
-        fread(b,1,1,fp);
-	fread(b+1,1,1,fp);
-	//fread(b,1,1,fp);			
-	printf("%x ",*(b+1));
+    for(int i=0;i<10;i++)
+    {
+	fread(b,1,1,fp);
+	printf("%x ",*b);
+    }
     
     fclose(fp);
 } 
